1f1b5cc75a7ebfb6450422fa60ca4da729de64bd
ekbal11 <31066203+ekbal11@users.noreply.github.com>
2021-08-01 22:20:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,33 +16,33 @@ function buttonInnerhtml(key){
 
     switch (key) {
         case "w":
-            var w=new Audio("sounds/tom-1.mp3");
+            var w=new Audio("tom-1.mp3");
             w.play();
         break;
 
         case "a":
-            var a=new Audio('sounds/tom-2.mp3');
+            var a=new Audio('tom-2.mp3');
             a.play();
         break;
 
         case "s":
-            new Audio("sounds/tom-3.mp3").play();
+            new Audio("tom-3.mp3").play();
         break;
 
         case "d":
-            new Audio("sounds/tom-4.mp3").play();
+            new Audio("tom-4.mp3").play();
         break;
         
         case "j":            
-            new Audio("sounds/snare.mp3").play();
+            new Audio("snare.mp3").play();
         break;
 
         case "k":
-            new Audio("sounds/crash.mp3").play();
+            new Audio("crash.mp3").play();
         break;
 
         case "l":
-            new Audio("sounds/kick-bass.mp3").play();
+            new Audio("kick-bass.mp3").play();
         break;
         
         default:alert(key);
